8b4c06581707f6d2277c7ab6adcbbe5f551cf385
Agianni21 <agustinborello21@gmail.com>
2019-04-29 10:43:17 -0300

    Setup config to use nvim as default system editor

--- setup.sh
+++ setup.sh
@@ -10,21 +10,28 @@ if ! type nvim &> /dev/null; then
     sudo apt-get install neovim
 fi
 
-# Vim config (neovim)
+# Set nvim as default editor
+# For bash
+echo "export EDITOR=nvim" >> ${HOME}/.bashrc
+# For fish
+echo "set -X EDITOR nvim" >> ${HOME}/.config/fish/config.fish
+
+
+# Setup vim config (neovim)
 if [[ ! -d ~/.config/nvim ]]; then
     mkdir ~/.config/nvim
 fi
 if [[ -e ~/.config/nvim/init.vim ]]; then
     rm ~/.config/nvim/init.vim
 fi
-
 ln ${work_dir}/.vimrc ~/.config/nvim/init.vim   
+
+# Setup Vundle as vim plugin manager and install plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 nvim +PluginInstall +qall
 
-# Tmux config
+# Setup tmux config
 if [[ -e ~/.tmux.conf ]]; then
     rm ~/.tmux.conf
 fi
-
 ln ${work_dir}/.tmux.conf ~/.tmux.conf
