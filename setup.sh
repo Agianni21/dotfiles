bf755ec723239a38382f9df51982506854e6b6ce
Agustin Gianni Borello <agustingianni_borello@mcafee.com>
2019-08-23 11:55:29 -0300

    New setup

--- setup.sh
+++ setup.sh
@@ -5,22 +5,34 @@
 # + Check if nvim is already the default editor
 
 work_dir="$(pwd)"
+setup_plugins="false"
 
-# Setup vim config (neovim)
+### Setup configs ###
+## Nvim
 if [[ ! -d ~/.config/nvim ]]; then
     mkdir ~/.config/nvim
 fi
-if [[ -e ~/.config/nvim/init.vim ]]; then
-    rm ~/.config/nvim/init.vim
+
+if [[ ! -d ~/.config/nvim/colors ]]; then
+    mkdir ~/.config/nvim/colors
 fi
-ln ${work_dir}/.vimrc ~/.config/nvim/init.vim   
+
+cp ${work_dir}/vimrc ~/.config/nvim/init.vim   
+cp -r ${work_dir}/vim/colors ~/.config/nvim/
+
+## Vim
+if [[ ! -d ~/.vim ]]; then
+    mkdir ~/.vim
+fi
+
+cp ${work_dir}/vimrc ~/.vimrc
+cp -r ${work_dir}/vim/colors ~/.vim/
+
+## Tmux
+cp ${work_dir}/tmux.conf ~/.tmux.conf
 
 # Setup Vundle as vim plugin manager and install plugins
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-nvim +PluginInstall +qall
-
-# Setup tmux config
-if [[ -e ~/.tmux.conf ]]; then
-    rm ~/.tmux.conf
+if [[ ${setup_plugins} == "true" ]]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    nvim +PluginInstall +qall
 fi
-ln ${work_dir}/.tmux.conf ~/.tmux.conf
